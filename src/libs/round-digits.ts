2c9382a51d063eb26d5f89a5ff23b1b85ea337d8
Emeryao <emeryao@outlook.com>
2022-01-17 17:10:18 +0800

    fix: round digits fail for non number params

--- src/libs/round-digits.ts
+++ src/libs/round-digits.ts
@@ -1,11 +1,11 @@
 export function roundDigits(num: number, digits: number): number {
+    if (typeof num !== 'number' || typeof digits !== 'number') {
+        return num;
+    }
     if (!digits || digits <= 0) {
         return num;
     }
-    const digitsRound: number = Math.round(digits);
-    let factor: number = 1;
-    for (let i: number = 0; i < digitsRound; i++) {
-        factor *= 10;
-    }
+    const digitsRound: number = Math.round(digits) || 0;
+    const factor: number = 10 ** digitsRound;
     return Math.round(num * factor) / factor;
 }
