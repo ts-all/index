72b3c48f8eb6c00d9bdc013db7853bd08c1f9ed9
emeryao <emeryao@outlook.com>
2022-09-19 19:13:09 +0800

    feat: export type for env config base

--- src/libs/get-env-config.ts
+++ src/libs/get-env-config.ts
@@ -1,6 +1,8 @@
 import { constantCase } from './constant-case';
 
-export function getEnvConfig(envConfig: Record<string, unknown>, continueOnEmpty: boolean = false, printTemplate: boolean = false): void {
+export type EnvConfigBase = Record<string, unknown>;
+
+export function getEnvConfig(envConfig: EnvConfigBase, continueOnEmpty: boolean = false, printTemplate: boolean = false): void {
     if (!process.release.name) {
         return;
     }
@@ -9,9 +11,9 @@ export function getEnvConfig(envConfig: Record<string, unknown>, continueOnEmpty
             const envKeyName: string = constantCase(key);
 
             if (printTemplate) {
-                const hasValue: boolean = Boolean((envConfig)[key]);
+                const hasValue: boolean = Boolean(envConfig[key]);
                 if (hasValue) {
-                    console.info(`Z_${envKeyName}="${(envConfig)[key] as string}"`);
+                    console.info(`Z_${envKeyName}="${envConfig[key] as string}"`);
                 } else {
                     console.info(`${envKeyName}=""`);
                 }
@@ -19,14 +21,14 @@ export function getEnvConfig(envConfig: Record<string, unknown>, continueOnEmpty
 
             if (Reflect.has(process.env, envKeyName)) {
                 console.info(envKeyName, process.env[envKeyName]);
-                if (typeof (envConfig)[key] == 'string') {
-                    (envConfig)[key] = (process.env[envKeyName] ?? (envConfig)[key]) || '';
-                } else if (typeof (envConfig)[key] == 'number') {
-                    (envConfig)[key] = Number.parseFloat(process.env[envKeyName] ?? '') || (envConfig)[key];
+                if (typeof envConfig[key] == 'string') {
+                    envConfig[key] = (process.env[envKeyName] ?? envConfig[key]) || '';
+                } else if (typeof envConfig[key] == 'number') {
+                    envConfig[key] = Number.parseFloat(process.env[envKeyName] ?? '') || envConfig[key];
                 }
             }
 
-            if (!(envConfig)[key]) {
+            if (!envConfig[key]) {
                 console.error(`No ENV Variable found with name: ${envKeyName}`);
                 if (continueOnEmpty) {
                     continue;
